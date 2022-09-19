86fb7a4cafe76a5e17afbe2d2c82eb3d3505adab
YCChen <victor.chen@openapply.com>
2022-09-20 00:11:37 +0800

    add Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_152356) do
+ActiveRecord::Schema.define(version: 2022_09_19_161118) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
